a04d11738c5c4b8e893c45ae6aeb5edd66dfae19
Guy Cohen <gaico070@gmail.com>
2022-03-21 19:57:59 +0200

    .

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require("express")
+const app = express() // create an express app
+const morgan = require("morgan")
+
+const mongoose = require('mongoose')
+mongoose.connect(`mongodb+srv://Guy:ZeZPHn3HXcW4FAvx@article-api.oqplf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+mongoose.connection.on('connected',() => {
+    console.log('MongoDB Connected!')
+})
+const articlesRoutes = require('./api/routes/articles')
+const categoryRoutes = require('./api/routes/categories')
+const usersRoutes = require('./api/routes/users')
+
+app.use(morgan('dev'))
+
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: false
+}))
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origion", "*")
+    res.header("Access-Control-Allow-Header", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+    if(req.method === "OPTIONS"){
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET")
+        return res.status(200).json({})
+    }
+    next()
+})
+
+// Routes
+app.use('/articles',articlesRoutes)
+app.use('/categories',categoryRoutes)
+app.use('/users',usersRoutes)
+
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found')
+    error.status = 404
+    next(error)
+})
+
+// Error management
+app.use((error, req, res, next) => { 
+    res.status(error.status)
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+    next(error)
+})
+
+module.exports = app;
