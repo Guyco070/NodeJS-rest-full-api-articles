0438acf443a29d4c2b41817f7862bebd3bd62979
Guy Cohen <gaico070@gmail.com>
2022-03-23 08:14:56 +0200

    Signup and login for users

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,6 +1,7 @@
 const { default: mongoose } = require('mongoose')
 const User = require('../models/user')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 module.exports = {
     signup: (req,res) => {
@@ -11,7 +12,7 @@ module.exports = {
                 return res.status(409).json({
                     message: "Email exist"
                 })
-                
+
                 bcrypt.hash(password, 10, (error, hash) => {
                     if(error)
                         return res.status(500).json({
@@ -37,8 +38,34 @@ module.exports = {
             })
     },
     login: (req,res) => {
-        res.status(200).json({
-            message: 'login'
+        const {email, password} = req.body
+
+        User.findOne({ email }).then((user) => {
+            if(!user)
+                return res.status(401).json({
+                    message: "Auth faild"
+                })
+            bcrypt.compare(password, user.password, (error, result) => {
+                if(error)
+                    return res.status(401).json({
+                        message: "Auth faild"
+                    })
+                if(result)
+                    return res.status(401).json({
+                        message: "Auth successfull",
+                        token: jwt.sign({
+                            id: user._id,
+                            email: user.email
+                        }, process.env.JWT_KEY,
+                        {
+                            expiresIn: "1H"
+                        })
+                    })
+                    
+                res.status(401).json({
+                    message: "Auth faild"
+                })
+            })
         })
     },
 }
